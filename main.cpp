7b13b3a40d4f58246048615dcd676544f88acc13
sanghuynh0929 <120449026+sanghuynh0929@users.noreply.github.com>
2022-12-28 21:05:13 +0700

    Change board size to 20x20

--- main.cpp
+++ main.cpp
@@ -5,43 +5,43 @@
 using namespace std;
 
 // Freestyle
-bool winning(int Board[15][15], int x, int y) {
+bool winning(int Board[20][20], int x, int y) {
     if(x < 0 or y < 0)
         return false;
     
     // Check row:
     // x-4 x-3 x-2 x-1 x x+1 x+2 x+3 x+4
     for(int i = x - 4; i <= x; i++) {
-        if(i < 0 or i + 4 >= 15) continue;
+        if(i < 0 or i + 4 >= 20) continue;
         if(Board[x][y] == Board[i][y] and Board[x][y] == Board[i+1][y] and Board[x][y] == Board[i+2][y] and Board[x][y] == Board[i+3][y] and Board[x][y] == Board[i+4][y]) return true;
     }
     // Check column:
     // y-4 y-3 y-2 y-1 y y+1 y+2 y+3 y+4
     for(int i = y - 4; i <= y; i++) {
-        if(i < 0 or i + 4 >= 15) continue;
+        if(i < 0 or i + 4 >= 20) continue;
         if(Board[x][y] == Board[x][i] and Board[x][y] == Board[x][i+1] and Board[x][y] == Board[x][i+2] and Board[x][y] == Board[x][i+3] and Board[x][y] == Board[x][i+4]) return true;
     }
     // Check diagonal
     // x-4 x-3 x-2 x-1 x x+1 x+2 x+3 x+4
     // y-4 y-3 y-2 y-1 y y+1 y+2 y+3 y+4
     for(int i = x-4, j = y-4; i <= x and j <= y; i++, j++) {
-        if(i < 0 or j < 0 or i + 4 >= 15 or j + 4 >= 15) continue;
+        if(i < 0 or j < 0 or i + 4 >= 20 or j + 4 >= 20) continue;
         if(Board[x][y] == Board[i][j] and Board[x][y] == Board[i+1][j+1] and Board[x][y] == Board[i+2][j+2] and Board[x][y] == Board[i+3][j+3] and Board[x][y] == Board[i+4][j+4]) return true;
     }
     // Check diagonal
     // x+4 x+3 x+2 x+1 x x-1 x-2 x-3 x-4
     // y-4 y-3 y-2 y-1 y y+1 y+2 y+3 y+4
     for(int i = x+4, j = y-4; i >= x and j <= y; i--, j++) {
-        if(i < 0 or j < 0 or i + 4 >= 15 or j + 4 >= 15) continue;
+        if(i < 0 or j < 0 or i + 4 >= 20 or j + 4 >= 20) continue;
         if(Board[x][y] == Board[i][j] and Board[x][y] == Board[i-1][j+1] and Board[x][y] == Board[i-2][j+2] and Board[x][y] == Board[i-3][j+3] and Board[x][y] == Board[i-4][j+4]) return true;
     }
     return false;
 }
 
-int Search(int Board[15][15], int player, int time, int xprev, int yprev) {
+int Search(int Board[20][20], int player, int time, int xprev, int yprev) {
     int xcount = 0, ocount = 0;
-    for(int i = 0; i < 15; i++) {
-        for(int j = 0; j < 15; j++) {
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
             if(Board[i][j] == 1) xcount++;
             if(Board[i][j] == -1) ocount++;
         }
@@ -50,9 +50,9 @@ int Search(int Board[15][15], int player, int time, int xprev, int yprev) {
         return -player;
         // Human wins
     }
-    if(xcount + ocount == 225) return 2;
+    if(xcount + ocount == 400) return 2;
     if(xcount + ocount == 0) {
-        Board[7][7] = player;
+        Board[10][10] = player;
         return 0;
     }
     Move bestmove = engine(Board, player, time);
@@ -65,16 +65,16 @@ int Search(int Board[15][15], int player, int time, int xprev, int yprev) {
     return 0;
 }
 
-void PrintBoard(int Board[15][15]){
+void PrintBoard(int Board[20][20]){
     system("clear");
-    cout << "                       1 1 1 1 1" << endl;
-    cout << "   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4" << endl;
-    for(int i = 0; i < 15; i++) {
+    cout << "                       1 1 1 1 1 1 1 1 1 1" << endl;
+    cout << "   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9" << endl;
+    for(int i = 0; i < 20; i++) {
         if(i < 10)
             cout << " " << i << " ";
         else
             cout << i << " ";
-        for(int j = 0; j < 15; j++) {
+        for(int j = 0; j < 20; j++) {
             if(Board[i][j] == 0)
                 cout << "_ ";
             else if (Board[i][j] == 1)
@@ -88,14 +88,13 @@ void PrintBoard(int Board[15][15]){
 }
 
 void Game(int player, int time) {
-    int Board[15][15] = {};
-    for(int i = 0; i < 15; i++) {
-        for(int j = 0; j < 15; j++) {
+    int Board[20][20] = {};
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
             Board[i][j] = 0;
         }
     }
     cout << "Press any key to start:" << endl;
-    system("pause");
     int x = -1, y = -1;
     while(true) {
         if(player == -1) {
@@ -146,15 +145,15 @@ void Game(int player, int time) {
 
 int main(int argc, const char * argv[]) {
     int player = 0;
-    int time = 15;
+    int time = 5;
     while(true) {
         cout << "Choose your color (X/O): ";
         string c; cin >> c;
-        if(c == "X"){
+        if(c == "X" or c == "x"){
             player = 1;
             break;
         }
-        if(c == "O"){
+        if(c == "O" or c == "o"){
             player = -1;
             break;
         }
@@ -163,7 +162,7 @@ int main(int argc, const char * argv[]) {
     string t; cin >> t;
     int tmp = stoi(t);
     if(tmp > 120 or tmp < 1) {
-        cout << "Time is set to default: 15s per move" << endl;
+        cout << "Time is set to default: 5s per move" << endl;
     }
     else {
         cout << "Time is set to " << tmp << "s per move" << endl;
